d1dc10c138917e2d5dd957e35736925fedcb43a9
Aayush-Th <thakuraayush633@gmail.com>
2026-09-05 22:43:39 +0530

    auth binding

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import "../auth.form.scss"
 import {useNavigate, Link} from "react-router"
 
+import { useAuth } from '../hooks/useAuth';
+
 const Login = () => {
+
+    const { loading, handleLogin } = useAuth();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Handle login logic here
-        navigate('/Register');
+        
+        handleLogin(email, password);
+       
     }
+        if(loading) {
+            return (<main><h1>Loading...</h1></main>)
+        }
+    
   return (
     <main>
         <div className="form-container">
@@ -19,13 +32,17 @@ const Login = () => {
 
                 <div className="input-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                    <input 
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    type="email" id="email" name="email" placeholder="Enter your email" required />
                    
                 </div>
                 
                 <div className="input-group">
                        <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                    <input 
+                    onChange={(e) => {setPassword(e.target.value)}}
+                    type="password" id="password" name="password" placeholder="Enter your password" required />
                 </div>
                 <button className="button primary-button" type="submit">Login</button>
             </form>
